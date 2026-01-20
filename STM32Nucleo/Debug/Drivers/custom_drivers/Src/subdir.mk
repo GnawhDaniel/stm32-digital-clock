e6826c583e1826a8339bf564626463ffaaf445c0
Daniel Hwang <danielhwangdeveloper@gmail.com>
2026-01-19 23:46:56 -0600

    init

---
+++ STM32Nucleo/Debug/Drivers/custom_drivers/Src/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/custom_drivers/Src/ds1307.c \
+../Drivers/custom_drivers/Src/esp8266_driver.c \
+../Drivers/custom_drivers/Src/lcd.c 
+
+OBJS += \
+./Drivers/custom_drivers/Src/ds1307.o \
+./Drivers/custom_drivers/Src/esp8266_driver.o \
+./Drivers/custom_drivers/Src/lcd.o 
+
+C_DEPS += \
+./Drivers/custom_drivers/Src/ds1307.d \
+./Drivers/custom_drivers/Src/esp8266_driver.d \
+./Drivers/custom_drivers/Src/lcd.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/custom_drivers/Src/%.o Drivers/custom_drivers/Src/%.su Drivers/custom_drivers/Src/%.cyclo: ../Drivers/custom_drivers/Src/%.c Drivers/custom_drivers/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32C031xx -c -I../Core/Inc -I"/home/danie/Documents/Dev/Embedded/embedded-mcu-driver-dev/wifi-stm32nucleo/Drivers/custom_drivers" -I"/home/danie/Documents/Dev/Embedded/embedded-mcu-driver-dev/wifi-stm32nucleo/Drivers/custom_drivers/Inc" -I"/home/danie/Documents/Dev/Embedded/embedded-mcu-driver-dev/wifi-stm32nucleo/Drivers/custom_drivers/Src" -I../Drivers/STM32C0xx_HAL_Driver/Inc -I../Drivers/STM32C0xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32C0xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Drivers-2f-custom_drivers-2f-Src
+
+clean-Drivers-2f-custom_drivers-2f-Src:
+	-$(RM) ./Drivers/custom_drivers/Src/ds1307.cyclo ./Drivers/custom_drivers/Src/ds1307.d ./Drivers/custom_drivers/Src/ds1307.o ./Drivers/custom_drivers/Src/ds1307.su ./Drivers/custom_drivers/Src/esp8266_driver.cyclo ./Drivers/custom_drivers/Src/esp8266_driver.d ./Drivers/custom_drivers/Src/esp8266_driver.o ./Drivers/custom_drivers/Src/esp8266_driver.su ./Drivers/custom_drivers/Src/lcd.cyclo ./Drivers/custom_drivers/Src/lcd.d ./Drivers/custom_drivers/Src/lcd.o ./Drivers/custom_drivers/Src/lcd.su
+
+.PHONY: clean-Drivers-2f-custom_drivers-2f-Src
+
---
+++ server/server.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify
+from datetime import datetime
+import pytz
+
+app = Flask(__name__)
+
+@app.route('/time', methods=['GET'])
+def get_time():
+    # Get current time in different formats
+    now = datetime.now()
+    
+    return jsonify({
+        'datetime': now.strftime('%Y-%m-%d %H:%M:%S'),
+        'date': now.strftime('%Y-%m-%d'),
+        'time': now.strftime('%H:%M:%S'),
+        'timestamp': int(now.timestamp()),
+        'timezone': 'America/Chicago'  # Your timezone
+    })
+
+@app.route('/time/utc', methods=['GET'])
+def get_utc_time():
+    # Get UTC time
+    utc_now = datetime.now(pytz.UTC)
+    
+    return jsonify({
+        'datetime': utc_now.strftime('%Y-%m-%d %H:%M:%S'),
+        'timestamp': int(utc_now.timestamp()),
+        'timezone': 'UTC'
+    })
+
+if __name__ == '__main__':
+    # Run on all interfaces so ESP can access it
+    app.run(host='0.0.0.0', port=5000, debug=True)
