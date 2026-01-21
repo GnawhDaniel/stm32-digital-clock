5b9bee28dc215a57ffa0e0a30b06d01c64cedf90
Daniel Hwang <danielhwangdeveloper@gmail.com>
2026-01-21 17:44:48 -0600

    moved blocking logic out of interrupts into main while loop.

--- STM32Nucleo/Core/Src/stm32c0xx_hal_msp.c
+++ STM32Nucleo/Core/Src/stm32c0xx_hal_msp.c
@@ -171,7 +171,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
     /* Peripheral clock enable */
     __HAL_RCC_TIM3_CLK_ENABLE();
     /* TIM3 interrupt Init */
-    HAL_NVIC_SetPriority(TIM3_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(TIM3_IRQn, 1, 0);
     HAL_NVIC_EnableIRQ(TIM3_IRQn);
     /* USER CODE BEGIN TIM3_MspInit 1 */
 
@@ -185,7 +185,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
     /* Peripheral clock enable */
     __HAL_RCC_TIM14_CLK_ENABLE();
     /* TIM14 interrupt Init */
-    HAL_NVIC_SetPriority(TIM14_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(TIM14_IRQn, 1, 0);
     HAL_NVIC_EnableIRQ(TIM14_IRQn);
     /* USER CODE BEGIN TIM14_MspInit 1 */
 
